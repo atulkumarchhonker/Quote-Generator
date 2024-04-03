34f4cb825f638af9f1a7c67a46efee7d9320f1c6
ATUL KUMAR <138485608+atulkumarchhonker@users.noreply.github.com>
2024-04-03 10:04:29 +0530

    upload

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// Solicita información a la API y muestra la frase  y el autor
+async function getapi() {
+    const response = await fetch("https://api.quotable.io/random");
+  
+    let datos = await response.json();
+    let frase = datos["content"];
+    let autor = datos["author"];
+  
+    document.getElementsByTagName("h1")[0].textContent = frase;
+    document.getElementsByTagName("h2")[0].textContent = autor;
+  }
+  
+  getapi();
+  document.getElementsByTagName("button")[0].addEventListener("click", getapi);
+  
